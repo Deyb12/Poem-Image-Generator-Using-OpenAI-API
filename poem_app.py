3bfa8260b388e91b02e9cbf7ffb2bcec9353e11e
Deyb <82894602+Deyb12@users.noreply.github.com>
2023-04-10 14:37:10 +0800

    Add files via upload

---
+++ poem_app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import openai
+
+# Set up OpenAI API key
+openai.api_key = "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"
+
+# Function to generate an image using OpenAI
+def generate_image(input_string): 
+    response = openai.Image.create(
+        prompt=input_string,
+        n=1,
+        size="256x256"
+    )
+    image_url = response['data'][0]['url']
+    return image_url
+
+# Function to generate a poem using OpenAI's GPT-3 language model
+def generate_poem(input_string):
+    prompt = f"Write a title about {input_string} and poem about {input_string}\n\n"
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    poem = response.choices[0].text.strip()
+    return poem
+
+gif_url = "https://thumbs.gfycat.com/TenseIncomparableAfricanfisheagle-size_restricted.gif"
+with st.columns(3)[1]:
+    st.image(gif_url, width=250)
+st.markdown("<h1 style='font-size: 42px; font-family: Courier New;'>Welcome to the <span style='color: #D4AF37;'>AI Poet</span>!</h1><p style = 'font-size: 18px;'>Please enter a few words that best describe how you envision your poem to look and sound. Once you have provided the information, you can press enter button in your keyboard to generate your poem instantly.</p><br>", unsafe_allow_html=True)
+input_string = st.text_input("Enter a topic for your poem and image:")
+
+if input_string:
+    st.subheader("Generated Poem with Image:")
+    
+    if "input_string" not in st.session_state or st.session_state.input_string != input_string:
+        st.session_state.input_string = input_string
+        st.session_state.image = generate_image(input_string)
+        st.session_state.poem = generate_poem(input_string)
+    
+    if st.button("Regenerate Image"):
+        image = generate_image(input_string)
+        st.session_state.image = image
+    else:
+        image = st.session_state.image
+    
+    if st.button("Regenerate Poem"):
+        poem = generate_poem(input_string)
+        st.session_state.poem = poem
+    else:
+        poem = st.session_state.poem
+    st.markdown(f"""
+    <div style='border: 1px solid black; padding: 10px;'>
+        <div style='display: flex; justify-content: center;'>
+            <img src='{image}' width=300>
+        </div>
+        <p style='text-align: center; font-size: 18px; font-family: Monotype Baskerville; margin-top: 20px;'>{poem}</p>
+    </div>
+""", unsafe_allow_html=True)
