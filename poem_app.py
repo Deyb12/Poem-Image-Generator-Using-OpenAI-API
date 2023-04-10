7e414d13fd7e55fd75bc82b03fd23daeab602887
Deyb <82894602+Deyb12@users.noreply.github.com>
2023-04-10 14:44:22 +0800

    Update poem_app.py

--- poem_app.py
+++ poem_app.py
@@ -61,3 +61,34 @@ if input_string:
         <p style='text-align: center; font-size: 18px; font-family: Monotype Baskerville; margin-top: 20px;'>{poem}</p>
     </div>
 """, unsafe_allow_html=True)
+    
+    
+
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://github.com/Deyb12" target="_blank">DAVE FAGARITA</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
